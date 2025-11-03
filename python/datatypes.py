3486211fb5fac757a7fc3018771da5590132e443
INGENIO <ingeniokhalif@gmail.com>
2025-11-03 17:10:15 +0100

    add datatypes and variables Python scripts

---
+++ python/datatypes.py
@@ -0,0 +1,35 @@
+# Strings
+str1 = "alpha"
+str2 = 'beta'
+str3 = """gamma"""
+str4 = '''delta'''
+
+print(str1, str2, str3, str4)
+
+# Numbers
+num1 = 100
+num2 = 99.99
+
+print(num1, num2)
+
+# Lists / Arrays
+first_list = [1, 2, str1, 4, "DevOps"]
+
+print(first_list)
+
+# Tuples
+first_tuple = (num1, num2, str2, "Python")
+
+print(first_tuple)
+
+# Dictionary
+first_dict = {1: "Python", 2: "DevOps", 3: "AWS"}
+
+print(first_dict)
+
+
+# Boolean
+bool1 = True
+bool2 = False
+
+print(bool1, bool2)
